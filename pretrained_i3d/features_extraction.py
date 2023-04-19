1d64ede59bd10d8362b3ebbec51043563b63b71b
simoclemens <117907132+simoclemens@users.noreply.github.com>
2023-04-19 22:13:29 +0200

    Implementation of features_extraction

--- pretrained_i3d/features_extraction.py
+++ pretrained_i3d/features_extraction.py
@@ -10,9 +10,4 @@ PATH = "rgb_imagenet.pt"
 model = torch.load(PATH)  # load the model from the pretrained i3d
 model.eval()  # set the model to evaluation mode
 
-dataset = utils.loaders.EpicKitchensDataset('RGB','test',)
-
-with torch.no_grad():
-    output = model(input_batch)
-
 
