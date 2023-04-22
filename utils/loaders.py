81e8910c80aa3ba78b02f66669cd3eb786f48999
Alessandro Caruso <92364264+al3ssandrocaruso@users.noreply.github.com>
2023-04-22 16:50:13 +0200

    added ".reshape(-1,1)" in output get indices

--- utils/loaders.py
+++ utils/loaders.py
@@ -100,7 +100,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
             for clip in clips:
                 output.append(dense_sampling(clip[0], clip[1], num_frames_per_clip, stride))
 
-        return np.array(output)
+        return np.array(output).reshape(-1,1)
 
     def _get_val_indices(self, record, modality='RGB', sampling='uniform', num_clip=5, clip_length=10,
                            num_frames_per_clip=5, stride = 2):
@@ -130,7 +130,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
             for clip in clips:
                 output.append(dense_sampling(clip[0], clip[1], num_frames_per_clip, stride))
 
-        return np.array(output)
+        return np.array(output).reshape(-1,1)
 
     def __getitem__(self, index):
 
